9b273860e7e57191a4fc2abbd1cfd9b5a277bfb6
Raluca Abalasei <105587057+raluus@users.noreply.github.com>
2023-02-27 19:55:46 +0200

    Added new field ProductCategory

---
+++ ShopManagementSystem/Migrations/20230227175235_ProductCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductCategory",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductCategory",
+                table: "Product");
+        }
+    }
+}
--- ShopManagementSystem/Models/Product.cs
+++ ShopManagementSystem/Models/Product.cs
@@ -3,6 +3,7 @@
     public class Product
     {
         public int Id { get; set; }
+        public string ProductCategory{ get; set; } = string.Empty;
         public string? ProductName { get; set; }
         public decimal Price { get; set; }
     }

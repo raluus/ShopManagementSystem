5da98f467fb915a46f63373a1e4c129a0105f36c
Raluca Abalasei <105587057+raluus@users.noreply.github.com>
2023-06-07 03:13:22 +0300

    Added styling for index and details admin pages

--- ShopManagementSystem/Pages/Account/EditUserProfile.cshtml.cs
+++ ShopManagementSystem/Pages/Account/EditUserProfile.cshtml.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using ShopManagementSystem.Data;
 using ShopManagementSystem.Models;
 
 namespace ShopManagementSystem.Pages.Account
@@ -49,9 +43,6 @@ namespace ShopManagementSystem.Pages.Account
             return Page();
         }
 
-
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)

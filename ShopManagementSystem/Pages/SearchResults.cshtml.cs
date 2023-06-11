089d58a5e7337387fa94a93ca6e71beae7a40dcd
Raluca Abalasei <105587057+raluus@users.noreply.github.com>
2023-06-11 03:44:02 +0300

    Added functionality for search button

---
+++ ShopManagementSystem/Pages/SearchResults.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ShopManagementSystem.Data;
+using ShopManagementSystem.Models;
+
+namespace ShopManagementSystem.Pages
+{
+    public class SearchResultsModel : PageModel
+    {
+        private readonly ShopManagementSystem.Data.ShopManagementSystemContext _context;
+        public string? SearchString { get; set; }
+
+        public SearchResultsModel(ShopManagementSystem.Data.ShopManagementSystemContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public List<Product> Product { get; set; } = default!;
+
+        [BindProperty]
+        public List<ProductInventory> ProductInventory { get; set; } = default!;
+
+
+        public async Task OnGetAsync()
+        {
+            //if (_context.Product != null)
+            //{
+            //    Product = await _context.Product.ToListAsync();
+            //}
+        }
+
+        public async Task<IActionResult> OnPostSearchResult()
+        {
+            SearchString = Request.Form["searchString"];
+            var products = from m in _context.Product
+                          select m;
+            var productInventory = from m in _context.ProductInventory select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                products = products.Where(s => s.ProductName.Contains(SearchString));
+            }
+
+            Product = await products.ToListAsync();
+            ProductInventory = await productInventory.ToListAsync();
+
+            return Page();
+        }
+    }
+}
